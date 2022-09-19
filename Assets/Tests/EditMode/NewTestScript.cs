9954ff2b454aa799c83b7f59d15d317f2a28452e
usmo <baduuadajab@hotmail.com>
2022-09-19 19:09:07 +0300

    #8 Create basic files for Unitys test runner

---
+++ Assets/Tests/EditMode/NewTestScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class NewTestScript
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void NewTestScriptSimplePasses()
+    {
+        // Use the Assert class to test condition
+        int counter = 1;
+        counter--;
+        // Assert.AreEqual(expected value, given value)
+        Assert.AreEqual(0, counter);
+    }
+
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    [UnityTest]
+    public IEnumerator NewTestScriptWithEnumeratorPasses()
+    {
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
+        yield return null;
+    }
+}
