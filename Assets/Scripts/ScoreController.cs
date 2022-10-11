c215a3a8595b4edcf61789c918958551285c14f4
oromiko <olli.roihupalo@tuni.fi>
2022-10-11 10:37:12 +0300

    #12 Score controller script

---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreController : MonoBehaviour
+{
+    public double score = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
