9ffe78b1603f3c27a267fdfcb914581e9e4c00f2
usmo <baduuadajab@hotmail.com>
2022-10-25 11:01:24 +0300

    #16 Added player animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public CharacterController controller;
+    public Animator animator;
 
     public float jumpForce = 400f;
 
@@ -14,11 +15,22 @@ public class Player : MonoBehaviour
 
     float horizontalMove = 0f;
     bool jump = false;
+    bool lookingRight = true;
+
 
     void Update()
     {
         // Check inputs and update variables
         horizontalMove = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("speed", Mathf.Abs(horizontalMove));
+        if(lookingRight && horizontalMove < 0)
+        {
+            Flip();
+        }
+        else if(!lookingRight && horizontalMove > 0)
+        {
+            Flip();
+        }
 
         if (Input.GetButtonDown("Jump"))
         {
@@ -30,7 +42,29 @@ public class Player : MonoBehaviour
     {
         // Move our character
         controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
+        if (jump)
+        {
+            animator.SetBool("isJumping", true);
+        }
+        
+        
         jump = false;
+    }
+
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        lookingRight = !lookingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    public void onLanding()
+    {
+        animator.SetBool("isJumping", false);
     }
 
 }
