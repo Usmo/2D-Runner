335049435a0f693cb099341357fb02f46d8e1974
rane <pessi.raunio@tuni.fi>
2022-11-01 15:17:58 +0200

    "#18 small addition to issue 18: Add score to win screen"

--- Assets/Scripts/GoalScript.cs
+++ Assets/Scripts/GoalScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class GoalScript : MonoBehaviour
 {
@@ -9,16 +10,18 @@ public class GoalScript : MonoBehaviour
     public GameObject WinScreen;
     public Player player;
     public ScoreController scoreController;
+    public Text scoreText;
 
     public void playerReachesGoal() 
     {
+    
         Time.timeScale = 0f;
         WinScreen.SetActive(true);
         if (scoreController != null) 
         {
             scoreController.addScore(scoreController.timeScore);
+            scoreText.text = "Score: " + scoreController.score.ToString(); //Show score on screen.
         }
-        
     }
 
     public void OnCollisionEnter2D(Collision2D other) 
