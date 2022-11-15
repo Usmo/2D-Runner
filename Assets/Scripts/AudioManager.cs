75c1e8c361264c1db44cfa88347f5f9163b4c66f
usmo <baduuadajab@hotmail.com>
2022-11-15 14:36:11 +0200

    #77 Jump and hurt sounds, small player texture fix

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    
+    public Sound[] sounds;
+
+    void Awake()
+    {
+        foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+
+
+        }
+    }
+
+    public void Play(String name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if(s != null)
+        {
+            s.source.Play();
+        }
+        else
+        {
+            Debug.LogWarning("Sound with name: " + name +  " not found!");
+        }
+    } 
+
+}
